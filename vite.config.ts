2e34448b2d02eb96af79e3dea57c7c67d1f7cfda
relliv <epsilonelittr@gmail.com>
2023-11-02 09:29:32 +0300

    Install react-router-dom and vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import tailwindcss from "tailwindcss"
+import Checker from "vite-plugin-checker"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Checker({ typescript: true })],
 
   server: {
-    port: 3000,
+    port: 3000
   },
 
   css: {
     postcss: {
-      plugins: [tailwindcss],
-    },
-  },
-});
+      plugins: [tailwindcss]
+    }
+  }
+})
