584bb980c91d8c8c10d601963f62c101bae0a8a2
relliv <epsilonelittr@gmail.com>
2023-11-02 09:33:25 +0300

    Add sass support

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { mainRouteList } from "./app/routes/main-routes"
+import "./App.scss"
 
 function App() {
   return (
